6f128edc822d32bb34d6b9e9c44c26bfa793ff9d
luminan-dev <pardaevasadbek02@gmail.com>
2025-08-13 14:27:18 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-const swiper = new Swiper('.swiper', {
-      slidesPerView: 3,
+const swiper = new Swiper('.info-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: { // telefon
+      slidesPerView: 1,
+      spaceBetween: 20,
+
+    },
+    768: { // planshet va kattaroq ekranlar
+      slidesPerView: 1,
       spaceBetween: 30,
-      allowTouchMove: false,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        767: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-          allowTouchMove: true,
-        }
-      }
-    });
+    }
+  }
+});
+
